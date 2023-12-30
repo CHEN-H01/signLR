42ff85032cac495500442c80fb34dc98c6541e7d
ceb <eb.cai@computer.com>
2023-12-30 21:17:57 +0800

    change UI

--- sign1/sign1/ContentView.swift
+++ sign1/sign1/ContentView.swift
@@ -5,10 +5,11 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(spacing: 20) {
                 Button("Open Camera") {
                     showCamera = true
                 }
+                .buttonStyle(CaringButtonStyle())
                 .sheet(isPresented: $showCamera) {
                     CameraViewAdapter()
                 }
@@ -16,7 +17,24 @@ struct ContentView: View {
                 Button("Upload Video") {
                     // 这里添加上传视频的逻辑
                 }
+                .buttonStyle(CaringButtonStyle())
             }
+            .padding()
+            .navigationTitle("Sath")
         }
+    }
+}
+
+
+// 自定义的关爱主题按钮样式
+struct CaringButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(configuration.isPressed ? Color.green.opacity(0.7) : Color.green)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+            .font(.title)
     }
 }
