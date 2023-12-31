0f540926d6874e8422d6e80879411a942f35fbdc
ceb <eb.cai@computer.com>
2023-12-31 16:17:43 +0800

    update background

--- sign1/sign1/ContentView.swift
+++ sign1/sign1/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
                 .buttonStyle(CaringButtonStyle())
             }
             .padding()
-            .background(Color(#colorLiteral(red: 0.95, green: 0.95, blue: 0.97, alpha: 1))) // 浅灰色背景
+            .background(PatternedBackground()) // 使用图案背景
             .navigationTitle("Sath")
         }
     }
@@ -31,11 +31,31 @@ struct CaringButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding()
-            .background(configuration.isPressed ? Color.blue.opacity(0.7) : Color.blue) // 浅蓝色按钮
+            .background(configuration.isPressed ? Color.blue.opacity(0.7) : Color.blue)
             .foregroundColor(.white)
-            .clipShape(Capsule()) // 圆角胶囊形状
+            .clipShape(Capsule())
             .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
             .font(.title)
-            .shadow(radius: 10) // 轻微的阴影
+            .shadow(radius: 10)
+    }
+}
+
+// 用于背景的图案
+struct PatternedBackground: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let size = geometry.size
+                let dotSize: CGFloat = 5
+                let spacing: CGFloat = 20
+
+                for x in stride(from: 0, through: size.width, by: spacing) {
+                    for y in stride(from: 0, through: size.height, by: spacing) {
+                        path.addEllipse(in: CGRect(x: x, y: y, width: dotSize, height: dotSize))
+                    }
+                }
+            }
+            .fill(Color.gray.opacity(0.2)) // 浅灰色的小圆点
+        }
     }
 }
