9dd06533681f4e21a2ea0828f3d9c07a9b609ce7
ceb <eb.cai@computer.com>
2023-12-31 17:12:36 +0800

    change background

--- sign1/sign1/ContentView.swift
+++ sign1/sign1/ContentView.swift
@@ -4,24 +4,29 @@ struct ContentView: View {
     @State private var showCamera = false
 
     var body: some View {
-        NavigationView {
-            VStack(spacing: 20) {
-                Button("Open Camera") {
-                    showCamera = true
-                }
-                .buttonStyle(CaringButtonStyle())
-                .sheet(isPresented: $showCamera) {
-                    CameraViewAdapter()
-                }
+        ZStack {
+            // 自定义背景
+            CustomBackground()
+                .edgesIgnoringSafeArea(.all)
 
-                Button("Upload Video") {
-                    // Add video upload logic here
+            NavigationView {
+                VStack(spacing: 20) {
+                    Button("Open Camera") {
+                        showCamera = true
+                    }
+                    .buttonStyle(CaringButtonStyle())
+                    .sheet(isPresented: $showCamera) {
+                        CameraViewAdapter()
+                    }
+
+                    Button("Upload Video") {
+                        // 添加上传视频的逻辑
+                    }
+                    .buttonStyle(CaringButtonStyle())
                 }
-                .buttonStyle(CaringButtonStyle())
+                .padding()
+                .navigationTitle("My App")
             }
-            .padding()
-            .background(WavePatternBackground()) // 使用波浪线图案背景
-            .navigationTitle("Sath")
         }
     }
 }
@@ -31,32 +36,30 @@ struct CaringButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding()
-            .background(configuration.isPressed ? Color.blue.opacity(0.7) : Color.blue)
+            .background(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.7), Color.blue]), startPoint: .top, endPoint: .bottom))
             .foregroundColor(.white)
-            .clipShape(Capsule())
+            .clipShape(RoundedRectangle(cornerRadius: 15))
             .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
             .font(.title)
-            .shadow(radius: 10)
     }
 }
 
-// 波浪线图案背景
-struct WavePatternBackground: View {
+// 自定义背景视图
+struct CustomBackground: View {
     var body: some View {
         GeometryReader { geometry in
-            Path { path in
-                let width = geometry.size.width
-                let height = geometry.size.height
-
-                // 创建波浪线
-                path.move(to: CGPoint(x: 0, y: height * 0.5))
-                for x in stride(from: 0, through: width, by: 40) {
-                    let relativeX = x / width
-                    let y = height * 0.5 + sin(relativeX * 2 * .pi) * 20
-                    path.addLine(to: CGPoint(x: x, y: y))
-                }
+            let size = geometry.size
+            VStack {
+                Spacer()
+                // 底部的渐变条
+                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.925, green: 0.941, blue: 0.945, alpha: 1)), Color(#colorLiteral(red: 0.878, green: 0.925, blue: 0.941, alpha: 1))]), startPoint: .leading, endPoint: .trailing)
+                    .frame(height: size.height * 0.1)
+                    .edgesIgnoringSafeArea(.bottom)
             }
-            .stroke(Color.blue.opacity(0.5), lineWidth: 2)
+            .background(
+                // 主要的渐变背景
+                RadialGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 0.984, blue: 0.965, alpha: 1)), Color(#colorLiteral(red: 0.804, green: 0.882, blue: 0.941, alpha: 1))]), center: .center, startRadius: 2, endRadius: 650)
+            )
         }
     }
 }
