10131eab971aa973c30deb6a79d311e72afcc1d9
ceb <eb.cai@computer.com>
2023-12-31 15:55:31 +0800

    update background

--- sign1/sign1/ContentView.swift
+++ sign1/sign1/ContentView.swift
@@ -15,26 +15,27 @@ struct ContentView: View {
                 }
 
                 Button("Upload Video") {
-                    // 这里添加上传视频的逻辑
+                    // Add video upload logic here
                 }
                 .buttonStyle(CaringButtonStyle())
             }
             .padding()
+            .background(Color(#colorLiteral(red: 0.95, green: 0.95, blue: 0.97, alpha: 1))) // 浅灰色背景
             .navigationTitle("Sath")
         }
     }
 }
-
 
 // 自定义的关爱主题按钮样式
 struct CaringButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding()
-            .background(configuration.isPressed ? Color.green.opacity(0.7) : Color.green)
+            .background(configuration.isPressed ? Color.blue.opacity(0.7) : Color.blue) // 浅蓝色按钮
             .foregroundColor(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 15))
+            .clipShape(Capsule()) // 圆角胶囊形状
             .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
             .font(.title)
+            .shadow(radius: 10) // 轻微的阴影
     }
 }
